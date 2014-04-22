99f9a333152767572679fe4bc9f2d8fec18c679b
Jared Smith <jared.smith.jrod@gmail.com>
2014-04-22 14:32:25 -0700

    First Commit

---
+++ scripts/sampledata.sql
@@ -0,0 +1,64 @@
+use `testing`;
+
+# User example
+#delete from `testing`.`user` where `first_name`='Jared';
+
+INSERT INTO `testing`.`user` (
+	`first_name`, `last_name`, 
+	`birth_date`, 
+	`not_a_password`, `email_address`, 
+	`city`, `country`) 
+VALUES (
+	'Jared', 'Smith', 
+	'1991-06-30', 'willhashandsaltlater', 
+	'jaredsmith@persona.ca', 
+	'victoria', 'canada');
+
+
+# Artist example
+#delete from `testing`.`artist` where `artist_name`='in my jaaaaaag';
+
+insert into`testing`. `artist` (
+	`artist_name`, `root_dir`)
+values (
+	'in my jaaaaaag', 'artistName');
+ 
+# Album example
+#delete from `testing`.`album` where `album_title` = '... on that bombshell';
+
+insert into `testing` . `album` (
+	`artist_id`,
+	`album_title`, `album_price`,
+	`tags`, `date_of_release`,
+	`dir_location`,
+	`description`)
+values (
+	'1',
+	'... on that bombshell', '900000',
+	'power', '2014-06-10',
+	'artistName/albumName',
+	'Usually involves irony, maybe write the description out to a fill and refer from that');
+
+
+# Song example
+#delete from `testing`.`song` where `song_title`='Good Night';
+
+insert into `testing` . `song` (
+	`album_id`, `song_number`,
+	`song_title`,  `song_price`,
+	`file_name`)
+values (
+	'1', '1',
+	'good night', '100',
+	'good_night.mp3');
+
+
+# Receipt example
+insert into `testing` . `receipt` (
+	`user_id`, `total_amount`,
+	`items`)
+values (
+	'1', '900000',
+	'1');
+
+select * from receipt;
