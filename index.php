1a41256f347671583e0670af1d2f4c127c61ae6a
00samer <samabev@gmail.com>
2014-05-13 19:49:24 -0700

    fixed index
    had wrong modal thingamajig

--- index.php
+++ index.php
@@ -11,6 +11,88 @@
     <link href="css/cartButton.css" rel="stylesheet" type="text/css" />
     <link href="css/galleryGenerator.css" rel="stylesheet" type="text/css" />
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'/>
+    
+    		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>
+			var modal = (function(){
+				var 
+				method = {},
+				$overlay,
+				$modal,
+				$content,
+				$close;
+
+				// Center the modal in the viewport
+				method.center = function () {
+					var top, left;
+
+					top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+					left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+					$modal.css({
+						top:top + $(window).scrollTop(), 
+						left:left + $(window).scrollLeft()
+					});
+				};
+
+				// Open the modal
+				method.open = function (settings) {
+					$content.empty().append(settings.content);
+
+					$modal.css({
+						width: settings.width || 'auto', 
+						height: settings.height || 'auto'
+					});
+
+					method.center();
+					$(window).bind('resize.modal', method.center);
+					$modal.show();
+					$overlay.show();
+				};
+
+				// Close the modal
+				method.close = function () {
+					$modal.hide();
+					$overlay.hide();
+					$content.empty();
+					$(window).unbind('resize.modal');
+				};
+
+				// Generate the HTML and add it to the document
+				$overlay = $('<div id="overlay"></div>');
+				$modal = $('<div id="modal"></div>');
+				$content = $('<div id="content"></div>');
+				$close = $('<a id="close" href="#"></a>');
+
+				$modal.hide();
+				$overlay.hide();
+				$modal.append($content, $close);
+
+				$(document).ready(function(){
+					$('body').append($overlay, $modal);
+				});
+
+				$close.click(function(e){
+					e.preventDefault();
+					method.close();
+				});
+
+				return method;
+			}());
+
+			// Wait until the DOM has loaded before querying the document
+			$(document).ready(function(){
+
+ 			  $('a#login').click(function(e){
+
+                            $.get('ajax.php', function(data){
+					modal.open({content: data});
+                                        e.preventDefault();
+				});
+			  });
+                     });
+
+</script>
 
 </head>
 
@@ -100,27 +182,7 @@
         </div>
 
         <aside class="top-sidebar">
-
-            <div id='basic-modal'>
-                <input type='button' name='basic' value='Login' class='basic'/>
-            </div>
-
-            <!-- modal content -->
-            <div id="basic-modal-content">
-                <form action="scripts/php/process_login.php" method="post" name="login_form">Email<br>
-                    <input type="text" name="email" /><br><br>Password<br>
-                    <input type="password" name="password" id="password"/>  <br><br>
-                    <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
-                </form>
-                <p>If you don't have a login, please <a href="register.php">register</a></p>
-                <p>If you are done, please <a href="scripts/php/logout.php">log out</a>.</p>
-                <p>If you are logged in, you can view this <a href="protected_page.php">page</a></p>
-                <p>You are currently logged out.</p>
-            </div>
-
-            <div style='display:none'>
-                <img src='img/basic/x.png' alt='' />
-            </div>
+            <a id="login" href="#">Login</a>
 
         </aside>
 
