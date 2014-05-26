b4262f8935bc3eeee417e8d65ddc1b4978705863
00samer <samabev@gmail.com>
2014-05-26 16:34:15 -0700

    lil fixerooni
    pce

--- viewCart.php
+++ viewCart.php
@@ -15,12 +15,14 @@
         for($i = 0; $i < count($_SESSION['allAlbums']); $i++){
             $newAlbum = $_SESSION['allAlbums'][$i];
             echo $newAlbum;
-
+            echo "<br>";
             echo "<form id='form1' action='removeFromCart.php' method='post'>";
             echo "<a href='javascript:;' onclick='parentNode.submit();'>Remove</a>";
             echo "<input type='hidden' name='test3' value='".$newAlbum."' />";
             echo "</form";
               //'<a href= "removeFromCart.php" value="" class="removeCartButton" name="$_SESSION['allAlbums'][$i]">x</button>'
             echo "<br>";
+            echo "<br>";
+
         }
 ?>
