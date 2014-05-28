1c95ec0f70c83917e65d25434561f362b031d264
Jared Smith <jared.smith.jrod@gmail.com>
2014-05-28 16:09:45 -0700

    blah blah

--- myCart.php
+++ myCart.php
@@ -2,108 +2,109 @@
 <html lang="en">
 <head>
     <title>Tune Source</title>
-    <meta charset = "utf-8" />
-		
-    <link href="css/index-style.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8"/>
+
+    <link href="css/index-style.css" rel="stylesheet" type="text/css"/>
     <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
 
     <!-- Gallery Generator -->
-    <link href="css/buttons.css" rel="stylesheet" type="text/css" />
-    <link href="css/galleryGenerator.css" rel="stylesheet" type="text/css" />
+    <link href="css/buttons.css" rel="stylesheet" type="text/css"/>
+    <link href="js/jquery.qtip.css" rel="stylesheet" type="text/css"/>
+    <link href="css/galleryGenerator.css" rel="stylesheet" type="text/css"/>
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'/>
-    
-    		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<script>
-			var modal = (function(){
-				var 
-				method = {},
-				$overlay,
-				$modal,
-				$content,
-				$close;
 
-				// Center the modal in the viewport
-				method.center = function () {
-					var top, left;
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>
+        var modal = (function () {
+            var
+                method = {},
+                $overlay,
+                $modal,
+                $content,
+                $close;
 
-					top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
-					left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+            // Center the modal in the viewport
+            method.center = function () {
+                var top, left;
 
-					$modal.css({
-						top:top + $(window).scrollTop(),
-						left:left + $(window).scrollLeft()
-					});
-				};
+                top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+                left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
 
-				// Open the modal
-				method.open = function (settings) {
-					$content.empty().append(settings.content);
+                $modal.css({
+                    top: top + $(window).scrollTop(),
+                    left: left + $(window).scrollLeft()
+                });
+            };
 
-					$modal.css({
-						width: settings.width || 'auto', 
-						height: settings.height || 'auto'
-					});
+            // Open the modal
+            method.open = function (settings) {
+                $content.empty().append(settings.content);
 
-					method.center();
-					$(window).bind('resize.modal', method.center);
-					$modal.show();
-					$overlay.show();
-				};
+                $modal.css({
+                    width: settings.width || 'auto',
+                    height: settings.height || 'auto'
+                });
 
-				// Close the modal
-				method.close = function () {
-					$modal.hide();
-					$overlay.hide();
-					$content.empty();
-					$(window).unbind('resize.modal');
-				};
+                method.center();
+                $(window).bind('resize.modal', method.center);
+                $modal.show();
+                $overlay.show();
+            };
 
-				// Generate the HTML and add it to the document
-				$overlay = $('<div id="overlay"></div>');
-				$modal = $('<div id="modal"></div>');
-				$content = $('<div id="content"></div>');
-				$close = $('<a id="close" href="#"></a>');
+            // Close the modal
+            method.close = function () {
+                $modal.hide();
+                $overlay.hide();
+                $content.empty();
+                $(window).unbind('resize.modal');
+            };
 
-				$modal.hide();
-				$overlay.hide();
-				$modal.append($content, $close);
+            // Generate the HTML and add it to the document
+            $overlay = $('<div id="overlay"></div>');
+            $modal = $('<div id="modal"></div>');
+            $content = $('<div id="content"></div>');
+            $close = $('<a id="close" href="#"></a>');
 
-				$(document).ready(function(){
-					$('body').append($overlay, $modal);
-				});
+            $modal.hide();
+            $overlay.hide();
+            $modal.append($content, $close);
 
-				$close.click(function(e){
-					e.preventDefault();
-					method.close();
-				});
+            $(document).ready(function () {
+                $('body').append($overlay, $modal);
+            });
 
-				return method;
-			}());
+            $close.click(function (e) {
+                e.preventDefault();
+                method.close();
+            });
 
-			// Wait until the DOM has loaded before querying the document
-			$(document).ready(function(){
+            return method;
+        }());
 
- 			  $('a#login').click(function(e){
+        // Wait until the DOM has loaded before querying the document
+        $(document).ready(function () {
 
-                            $.get('ajax.php', function(data){
-					modal.open({content: data});
-                                        e.preventDefault();
-				});
-			  });
-                     });
+            $('a#login').click(function (e) {
 
-</script>
-<script>
-function formhash(form, fname,lname,country,province,address,postal,phone,credit,expiry){
-if (fname.value==''||lname.value==''||country.value==''||province.value==''||address.value==''
-||postal.value==''||phone.value==''||credit.value==''||expiry.value==''){
-  alert("Not all Forms have to be Filled");
-  return false;
-  }
-  form.submit()
-  return true;
-}
-</script>
+                $.get('ajax.php', function (data) {
+                    modal.open({content: data});
+                    e.preventDefault();
+                });
+            });
+        });
+
+    </script>
+    <script>
+        function formhash(form, fname, lname, country, province, address, postal, phone, credit, expiry) {
+            if (fname.value == '' || lname.value == '' || country.value == '' || province.value == '' || address.value == ''
+                || postal.value == '' || phone.value == '' || credit.value == '' || expiry.value == '') {
+                alert("Not all Forms have to be Filled");
+                return false;
+            }
+            form.submit()
+            return true;
+        }
+    </script>
 </head>
 <header class="header fixed bar" role="banner">
     <nav>
@@ -115,125 +116,127 @@ if (fname.value==''||lname.value==''||country.value==''||province.value==''||add
 
                 <div class="item">
                     <div class="searchBlock">
-                        <input type="search" placeholder="search" id="searchBar" />
+                        <input type="search" placeholder="search" id="searchBar"/>
+
                         <div id="searchButton">
-                            <button value="searchButton"><img id="searchImage" src="res/image/search.png"></img></button>
+                            <button value="searchButton"><img id="searchImage" src="res/image/search.png"></img>
+                            </button>
                         </div>
                     </div>
                 </div>
 
                 <div class="item">
-                    <div class="cartBlock"><a href="#cartMukery" id="whiteText" class="cartBox" title="<?php require_once "viewCart.php"; ?>Cart</a></div>
+                    <div class="cartBlock"><a href="#cartMukery" id="whiteText" class="cartBox"
+                                              title="<?php require_once "viewCart.php";?>">Cart</a></div>
                 </div>
 
-                <div class="item">
-                    <div class="loginBlock">
-                        <input type="text" id="userInput" name="username" placeholder="username"/>
-                        <input type="password" id="passInput" name="password" placeholder="password"/>
-                        <input type="submit" value="Login" />
+                <div class=" item">
+                        <div class="loginBlock">
+                            <input type="text" id="userInput" name="username" placeholder="username"/>
+                            <input type="password" id="passInput" name="password" placeholder="password"/>
+                            <input type="submit" value="Login"/>
+                        </div>
                     </div>
-                </div>
             </li>
         </ul>
     </nav>
 </header>
 <body>
-    <div class="contentBody">
+<div class="contentBody">
 
- <form name   = "menduorder" method = "post"
-       id     = "formbox"
-	   >
-       
-  <fieldset>      
-   <legend>checkout</legend>
-     <div class= "row">
-     <span class= "form_element">
+    <form name="menduorder" method="post"
+          id="formbox">
+
+        <fieldset>
+            <legend>checkout</legend>
+            <div class="row">
+     <span class="form_element">
       
-   <input type= "text" 
-          name= "firstname"
-          required id= "firstname"
-          placeholder= "Your First Name" >
+   <input type="text"
+          name="firstname"
+          required id="firstname"
+          placeholder="Your First Name">
           </span>
-        </div>
-            
-     <div class= "row">
-     <span class= "form_element"> 
-   <input type= "text" 
-          name= "lastname"
-          required id= "lastname"
-          placeholder= "Your Last Name" >
-          </span>
-        </div>
-	<div class= "row">
-    <span class= "form_element">	
-    <input type= "text" 
-          name= "Country"
-          required id= "Country"
-          placeholder= "Enter Your Country" >
-          </span>
-        </div>
-	
-	<div class= "row">
-    <span class= "form_element">
-	<input type= "text" 
-          name= "Province"
-          required id= "province"
-          placeholder= "Province Or State" >
-          </span>
-        </div>
-	
-	<div class= "row">
-    <span class= "form_element">	
-	<input type= "text" 
-          name= "Address"
-          required id= "Address"
-          placeholder= "Enter Your Address" >
-          </span>
-        </div>
-	
-	<div class= "row">
-    <span class= "form_element">	
-	<input type= "text" 
-          name= "postal"
-          required id= "postal"
-          placeholder= "Postal Code Or Zip" >
-          </span>
-        </div>
-            
-        <div class= "row">
-     <span class= "form_element">    
-   <input 
-          title= "Expected format as 250-999-9999"
-          type= "tel" 
-          name= "phone"
-          required id= "phone"
-          placeholder= "250-999-9999"
-          pattern="[\(]\d{3}[\)]\d{3}[\-]\d{4}">
-          </span>
-        </div> 
-            
-     <div class= "row">
-     <span class= "form_element"> 
-   <input type= "text" 
-          name= "credit"
-          required id= "credit"
-          placeholder= "Credit Card number"
-          pattern="[0-0]{4,6}">
-          </span>  
-        </div>        
- 
-     <div class= "row">
-     <span class= "form_element">        
-   <input type= "text" 
-          name= "expiry"
-          required id= "expiry"
-          placeholder= "Credit card expiry"
-          pattern= "\d{2}-\d{2}"
-          title= "Credit card expiry as MM-YY">
-          </span>
-        </div>
+            </div>
 
-		<input type="button" value="Checkout" onclick="return formhash(this.form, this.form.firstname,
+            <div class="row">
+     <span class="form_element">
+   <input type="text"
+          name="lastname"
+          required id="lastname"
+          placeholder="Your Last Name">
+          </span>
+            </div>
+            <div class="row">
+    <span class="form_element">
+    <input type="text"
+           name="Country"
+           required id="Country"
+           placeholder="Enter Your Country">
+          </span>
+            </div>
+
+            <div class="row">
+    <span class="form_element">
+	<input type="text"
+           name="Province"
+           required id="province"
+           placeholder="Province Or State">
+          </span>
+            </div>
+
+            <div class="row">
+    <span class="form_element">
+	<input type="text"
+           name="Address"
+           required id="Address"
+           placeholder="Enter Your Address">
+          </span>
+            </div>
+
+            <div class="row">
+    <span class="form_element">
+	<input type="text"
+           name="postal"
+           required id="postal"
+           placeholder="Postal Code Or Zip">
+          </span>
+            </div>
+
+            <div class="row">
+     <span class="form_element">
+   <input
+       title="Expected format as 250-999-9999"
+       type="tel"
+       name="phone"
+       required id="phone"
+       placeholder="250-999-9999"
+       pattern="[\(]\d{3}[\)]\d{3}[\-]\d{4}">
+          </span>
+            </div>
+
+            <div class="row">
+     <span class="form_element">
+   <input type="text"
+          name="credit"
+          required id="credit"
+          placeholder="Credit Card number"
+          pattern="[0-0]{4,6}">
+          </span>
+            </div>
+
+            <div class="row">
+     <span class="form_element">
+   <input type="text"
+          name="expiry"
+          required id="expiry"
+          placeholder="Credit card expiry"
+          pattern="\d{2}-\d{2}"
+          title="Credit card expiry as MM-YY">
+          </span>
+            </div>
+
+            <input type="button" value="Checkout" onclick="return formhash(this.form, this.form.firstname,
 														   this.form.lastname,
 														   this.form.Country,
 														   this.form.Province,
@@ -244,45 +247,62 @@ if (fname.value==''||lname.value==''||country.value==''||province.value==''||add
 														   this.form.expiry);"/>
 
 
-        </div>
+</div>
 
-    </div>
+</div>
 
 <!-- Load jQuery, SimpleModal and Basic JS files -->
 <script type='text/javascript' src='js/jquery.js'></script>
 <script type='text/javascript' src='js/jquery.simplemodal.js'></script>
 <script type='text/javascript' src='js/basic.js'></script>
 
-
 <script>
+    $('button[type=button]').click(function (e) {
+        var name = $(this).attr("name");
+        var albumName = $('button[name=' + name + ']').val();
+        $.post('addToCart.php', {name: albumName}, function (data) {
+            $('.cartBox').qtip({
+                show: true,
+                hide: 'click',
+                content: { text: data },
+                position: { adjust: { y: 13 }, my:'top center' }
+            });
+        });
+    });
 
-var itemCounter = <?php echo json_encode("removeFromCartButton$itemCounter") ?>;
-
- 			  $('button[type=button]').click(function(e){
-                            var test3 = $(this).attr("name");
-                            //var test4 = $('button[name=<?php echo json_encode("$newThing[0]") ?>]').val()
-                            var test5 = $('button[name='+test3+']').val();
-                            $.post('removeFromCart.php', {test3:test5}, function(data){
-					modal.open({content: data});
-                                        e.preventDefault();
-				});
-			  });
-			  
-			  $('button[type=submit]').click(function(e){
-                            //var test4 = $('button[name=<?php echo json_encode("$newThing[0]") ?>]').val()
-                            $.post('addToCart.php', function(data){
-					modal.open({content: data});
-                                        e.preventDefault();
-				});
-			  });
+    // Create the tooltips only when document ready
+    $(document).ready(function(){
+        // Show tooltip on all <a/> elements with title attributes, but only when
+        // clicked. Clicking again will hide it.
+        var a = "<?php echo $_SESSION['change']?>";
+        if (a < 1){
+            $('.cartBox').qtip({
+                show: 'click',
+                hide: 'click',
+                content: { url: 'viewCart.php' },
+                position: { adjust: { y: 13 }, my:'top center' }
+            });
+        } else {
+            $('.cartBox').qtip({
+                show: true,
+                hide: 'click',
+                content: { url: 'viewCart.php' },
+                position: { my:'top center'}
+            });
+        }
+        "<?php $_SESSION['change'] = 0?>"
+    });
 
 </script>
+<script type="text/javascript" src="js/jquery.qtip.min.js"></script>
+<script type="text/javascript" src="js/jquery.qtip.js"></script>
+<script type="text/javascript" src="js/imagesloaded.pkg.min.js"></script>
 
 </body>
 
 <footer class="footer fixed bar">
     <div>
-        <span id="whiteText">Copyright &copy; 2014</span><br />
+        <span id="whiteText">Copyright &copy; 2014</span><br/>
         <span id="whiteText">Authors: Sam Beveridge, Calvin Lam, Jared Smith</span>
     </div>
 </footer>
