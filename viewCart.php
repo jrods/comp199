7578091b6312380a964a2857658e01b0ee5f565b
Jared Smith <jared.smith.jrod@gmail.com>
2014-06-17 19:26:07 -0700

    ajax mostly done, but still not

--- viewCart.php
+++ viewCart.php
@@ -1,46 +1,37 @@
-
-<?php
-/*
-echo "<br>";
-
-echo "<br>";
-echo "<br>";
-
-echo "Cart: <br>";
-
-    $newAlbum = $_SESSION['allAlbums'][$i];
-    echo $newAlbum;
-    echo "<br>";
-    echo "<form id='form1' action='removeFromCart.php' method='post'>";
-    echo "<a href='javascript:;' onclick='parentNode.submit();'><b>Remove</b></a>";
-    echo "<input type='hidden' name='test3' value='" . $newAlbum . "' />";
-    echo "</form>";
-    echo "<br>";
-    echo "<br>";*/
-?>
 <html>
-
 <body>
-<?php
-@session_start();
+<div class="cart whiteText">
+    <div class="info">
+        <?php
+        @session_start();
 
-include("scripts/php/htmlGenerator.php");
+        include("scripts/php/htmlGenerator.php");
 
-if (!isset($_SESSION['allAlbums'])) {
-    $_SESSION['allAlbums'] = array();
-}
+        if (!isset($_SESSION['allAlbums'])) {
+            $_SESSION['allAlbums'] = array();
+        }
 
-$total = number_format((float)($_SESSION['cart']), 2, '.', '');
-echo divIdClass( "cartTotal", "cartTotal", sprintf("<h2>Total: $%s</h2>", $total));
+        $total = number_format((float)($_SESSION['cart']), 2, '.', '');
+        echo divIdClass("cartTotal", "cartTotal", sprintf("<h3>Total: $%s</h3>", $total));
 
-for ($i = 0; $i < count($_SESSION['allAlbums']); $i++) {
-    $newAlbum = $_SESSION['allAlbums'][$i];
-    echo $newAlbum;
-    echo "<br>";
-}
+        echo divIdClass("item", "itemTitle", "<h3>Items</h3>");
 
-//echo "<form action='myCart.php'><input type='submit' value='Checkout'></form>";
+        $allItems = "";
 
-?>
+        echo "<form id='form1' action='removeFromCart.php' method='post'>";
+
+        for ($i = 0; $i < count($_SESSION['allAlbums']); $i++) {
+            $newAlbum = $_SESSION['allAlbums'][$i];
+            echo "<span>$newAlbum</span>";
+            echo "<a href='javascript:;' onclick='parentNode.submit();' class=\"remove slightlyRed\">Remove</a><br>";
+            echo sprintf("<input type='hidden' name='test3' value='%s' />", $newAlbum);
+        }
+
+        echo "<form action='myCart.php'><input class='button whiteText' type='submit' value='Checkout'></form>";
+
+        ?>
+    </div>
+</div>
+
 </body>
 </html>
